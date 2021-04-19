8548d0963086e32aaffd6c0de76b4983eb43e85a
orsond <orsondmc@gmail.com>
2021-04-19 18:07:15 +1000

    do not print invites/joins/leaving for neary by groups

--- common/src/main/java/team/catgirl/collar/mod/common/features/Groups.java
+++ common/src/main/java/team/catgirl/collar/mod/common/features/Groups.java
@@ -1,6 +1,7 @@
 package team.catgirl.collar.mod.common.features;
 
 import team.catgirl.collar.api.groups.Group;
+import team.catgirl.collar.api.groups.GroupType;
 import team.catgirl.collar.api.session.Player;
 import team.catgirl.collar.client.Collar;
 import team.catgirl.collar.client.api.groups.GroupInvitation;
@@ -18,21 +19,33 @@ public class Groups implements GroupsListener {
 
     @Override
     public void onGroupCreated(Collar collar, GroupsApi groupsApi, Group group) {
+        if (group.type == GroupType.NEARBY) {
+            return;
+        }
         this.plastic.display.displayMessage(String.format("Created %s %s", group.type.name, group.name));
     }
 
     @Override
     public void onGroupJoined(Collar collar, GroupsApi groupsApi, Group group, Player player) {
+        if (group.type == GroupType.NEARBY) {
+            return;
+        }
         this.plastic.display.displayMessage(String.format("Joined %s %s", group.type.name, group.name));
     }
 
     @Override
     public void onGroupLeft(Collar collar, GroupsApi groupsApi, Group group, Player player) {
+        if (group.type == GroupType.NEARBY) {
+            return;
+        }
         this.plastic.display.displayMessage(String.format("Left %s %s", group.type.name, group.name));
     }
 
     @Override
     public void onGroupInvited(Collar collar, GroupsApi groupsApi, GroupInvitation invitation) {
+        if (invitation.type == GroupType.NEARBY) {
+            return;
+        }
         team.catgirl.plastic.player.Player player = plastic.world.allPlayers()
                 .stream().filter(player1 -> player1.id().equals(invitation.sender.minecraftPlayer.id))
                 .findFirst().orElseThrow(() -> new IllegalStateException("cannot find player " + invitation.sender.minecraftPlayer.id));
