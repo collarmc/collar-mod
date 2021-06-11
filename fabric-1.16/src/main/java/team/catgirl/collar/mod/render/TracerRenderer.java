4a398c393f1245230b1df03199a187cd9abd08f2
Orsond <orsondmc@gmail.com>
2021-06-11 16:11:28 +1000

    very quick tracer skid

--- fabric-1.16/src/main/java/team/catgirl/collar/mod/render/TracerRenderer.java
+++ fabric-1.16/src/main/java/team/catgirl/collar/mod/render/TracerRenderer.java
@@ -2,14 +2,15 @@ package team.catgirl.collar.mod.render;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.BufferRenderer;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.*;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.util.Window;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Vec3d;
+import org.joml.Vector3d;
 import org.joml.Vector3f;
 import org.lwjgl.opengl.GL11;
 import team.catgirl.collar.api.location.Location;
@@ -18,6 +19,8 @@ import team.catgirl.collar.mod.common.CollarService;
 import team.catgirl.plastic.Plastic;
 import team.catgirl.pounce.Preference;
 import team.catgirl.pounce.Subscribe;
+
+import java.awt.*;
 
 public final class TracerRenderer {
 
@@ -30,52 +33,63 @@ public final class TracerRenderer {
     }
 
     @Subscribe(Preference.CALLER)
-    public void onRender(RenderOverlaysEvent event) {
+    public void onRender(WorldRenderEvent event) {
+        ProjectionUtil.INSTANCE.updateBuffers();
+
+
         service.getCollar().ifPresent(collar -> {
             if (collar.getState() != Collar.State.CONNECTED || !collar.isDebug()) {
                 return;
             }
-            Window window = MinecraftClient.getInstance().getWindow();
-
-            float r = DyeColor.PINK.getColorComponents()[0];
-            float g = DyeColor.PINK.getColorComponents()[1];
-            float b = DyeColor.PINK.getColorComponents()[2];
-            float a = 0;
-
             collar.location().playerLocations().forEach((player, location) -> {
                 // Don't show where other players are if they are not in the same dimension
-                if (!location.dimension.equals(plastic.world.currentPlayer().location().dimension)) {
+                Location playerLocation = plastic.world.currentPlayer().location();
+                if (!location.dimension.equals(playerLocation.dimension)) {
                     return;
                 }
-                Vector3f feetPosition = lerpPos(location, event.deltaTime);
-                int screenWidth = window.getScaledWidth();
-                int screenHeight = window.getHeight();
-                GL11.glHint(GL11.GL_LINE_SMOOTH_HINT, GL11.GL_NICEST);
-                GL11.glEnable(GL11.GL_LINE_SMOOTH);
-                RenderSystem.enableBlend();
-                RenderSystem.disableTexture();
-                RenderSystem.defaultBlendFunc();
-
-                Tessellator tessellator = RenderSystem.renderThreadTesselator();
-                BufferBuilder buffer = tessellator.getBuffer();
-                buffer.begin(GL11.GL_LINE_STRIP, VertexFormats.POSITION_COLOR);
-                Matrix4f model = event.matrixStack.peek().getModel();
-                buffer.vertex(model, (screenWidth / 2), screenHeight / 2, 0).color(r, g, b, a).next();
-                buffer.vertex(model, feetPosition.x, feetPosition.y, 0).color(r, g, b, a).next();
-                buffer.end();
-                BufferRenderer.draw(buffer);
-                RenderSystem.enableTexture();
-                RenderSystem.disableBlend();
-                GL11.glDisable(GL11.GL_LINE_SMOOTH);
+                renderLine(from(playerLocation), from(location), Color.MAGENTA, 3f);
             });
         });
     }
 
-    private static Vector3f lerpPos(Location location, final float alpha) {
+    private Vec3d from(Location location) {
+        return new Vec3d(location.x, location.y, location.z);
+    }
+
+    private static void renderLine(Vec3d from, Vec3d to, Color col, float width) {
+        Camera c = BlockEntityRenderDispatcher.INSTANCE.camera;
+        GL11.glPushMatrix();
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glLineWidth(width);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glRotated(MathHelper.wrapDegrees(c.getPitch()), 1, 0, 0);
+        GL11.glRotated(MathHelper.wrapDegrees(c.getYaw() + 180.0), 0, 1, 0);
+        GL11.glColor4f(col.getRed() / 255F, col.getGreen() / 255F, col.getBlue() / 255F, col.getAlpha() / 255F);
+        GL11.glBegin(GL11.GL_LINES);
+        {
+            Vec3d f1 = from.subtract(c.getPos());
+            Vec3d t1 = to.subtract(c.getPos());
+            GL11.glVertex3d(f1.x, f1.y, f1.z);
+            GL11.glVertex3d(t1.x, t1.y, t1.z);
+        }
+        GL11.glEnd();
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glPopMatrix();
+    }
+
+    private static Vector3d lerpPos(Location location, final float alpha) {
         Location lastRenderLocation = location; // TODO: figuree out how to handle this
         float x = MathHelper.lerp(alpha, lastRenderLocation.x.floatValue(), location.x.floatValue());
         float y = MathHelper.lerp(alpha, lastRenderLocation.y.floatValue(), location.y.floatValue());
         float z = MathHelper.lerp(alpha, lastRenderLocation.z.floatValue(), location.z.floatValue());
-        return new Vector3f(x, y, z);
+        return new Vector3d(x, y, z);
     }
 }
