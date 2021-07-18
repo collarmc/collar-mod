52f8d20a71cf478bede2a49f7022f273f191382e
orsondmc <66061159+orsondmc@users.noreply.github.com>
2021-07-19 08:34:46 +1000

    Player Faces for map markers  (#7)

--- fabric-1.16/src/main/java/com/collarmc/plastic/fabric/FabricPlayer.java
+++ fabric-1.16/src/main/java/com/collarmc/plastic/fabric/FabricPlayer.java
@@ -1,7 +1,11 @@
 package com.collarmc.plastic.fabric;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
+import com.collarmc.api.location.Dimension;
+import com.collarmc.api.location.Location;
+import com.collarmc.mod.fabric.mixin.PlayerListEntryMixin;
+import com.collarmc.plastic.player.Player;
+import com.collarmc.plastic.ui.TextureProvider;
+import com.collarmc.plastic.ui.TextureType;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
@@ -9,31 +13,25 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.texture.NativeImageBackedTexture;
+import net.minecraft.resource.Resource;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import com.collarmc.api.location.Dimension;
-import com.collarmc.api.location.Location;
-import com.collarmc.mod.fabric.mixin.PlayerListEntryMixin;
-import com.collarmc.plastic.player.Player;
-import com.collarmc.plastic.ui.TextureProvider;
-import com.collarmc.plastic.ui.TextureType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 import static net.minecraft.world.dimension.DimensionType.*;
 
 public class FabricPlayer implements Player {
 
-    private final static Cache<String, Optional<BufferedImage>> AVATAR_CACHE = CacheBuilder.newBuilder()
-            .expireAfterAccess(60, TimeUnit.SECONDS)
-            .initialCapacity(50)
-            .build();
+    private static final Logger LOGGER = LogManager.getLogger(FabricPlayer.class);
 
     private final AbstractClientPlayerEntity playerEntity;
     private final TextureProvider textureProvider;
@@ -59,16 +57,13 @@ public class FabricPlayer implements Player {
     }
 
     @Override
-    public Optional<BufferedImage> avatar() {
-        try {
-            return AVATAR_CACHE.get(name(), () -> {
-                AtomicReference<BufferedImage> avatarImage = new AtomicReference<>();
-                textureProvider.getTexture(this, TextureType.AVATAR);
-                return avatarImage.get() == null ? Optional.empty() : Optional.of(avatarImage.get());
-            });
-        } catch (ExecutionException e) {
-            return Optional.empty();
-        }
+    public void avatar(Consumer<BufferedImage> consumer) {
+        textureProvider.getTexture(this, TextureType.AVATAR, defaultAvatar()).thenAccept(bufferedImageOptional -> {
+            bufferedImageOptional.ifPresent(consumer);
+            if (!bufferedImageOptional.isPresent()) {
+                LOGGER.error("Avatar for " + this + " is missing");
+            }
+        });
     }
 
     @Override
@@ -89,7 +84,7 @@ public class FabricPlayer implements Player {
             }
             Map<MinecraftProfileTexture.Type, Identifier> textures = entryMixin.textures();
             String textureName = String.format("plastic-capes/%s.png", playerEntity.getGameProfile().getId());
-            textureProvider.getTexture(this, TextureType.CAPE).thenAccept(textureOptional -> {
+            textureProvider.getTexture(this, TextureType.CAPE, null).thenAccept(textureOptional -> {
                 textureOptional.ifPresent(texture -> {
                     NativeImage image = nativeImageFrom(texture);
                     NativeImageBackedTexture nativeImageTexture = new NativeImageBackedTexture(image);
@@ -121,6 +116,21 @@ public class FabricPlayer implements Player {
         }
         BlockPos blockPos = playerEntity.getBlockPos();
         return new Location((double)blockPos.getX(), (double)blockPos.getY(), (double)blockPos.getZ(), dimension);
+    }
+
+    private BufferedImage defaultAvatar() {
+        MinecraftClient minecraftClient = MinecraftClient.getInstance();
+        if (minecraftClient == null) {
+            throw new IllegalStateException("minecraftClient");
+        }
+        Identifier skinTexture = this.playerEntity.getSkinTexture();
+        try {
+            Resource resource = minecraftClient.getResourceManager().getResource(skinTexture);
+            BufferedImage skin = ImageIO.read(resource.getInputStream());
+            return skin.getSubimage(8, 8, 15, 15);
+        } catch (IOException e) {
+            throw new IllegalStateException("could not load skin for " + this);
+        }
     }
 
     private static NativeImage nativeImageFrom(BufferedImage img) {
