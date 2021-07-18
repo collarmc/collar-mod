97bf74ff132333453cc1ea80592d802eb0afbaab
KosmX <kosmx.mc@gmail.com>
2021-07-18 11:41:17 +0200

    Waypoint events

--- common/src/main/java/com/collarmc/mod/common/features/events/WaypointCreatedEvent.java
+++ common/src/main/java/com/collarmc/mod/common/features/events/WaypointCreatedEvent.java
@@ -1,14 +1,15 @@
 package com.collarmc.mod.common.features.events;
 
 
+import com.collarmc.api.groups.Group;
 import com.collarmc.api.waypoints.Waypoint;
-
-import java.util.UUID;
 
 public final class WaypointCreatedEvent {
     public final Waypoint waypoint;
+    public final Group group;
 
-    public WaypointCreatedEvent(Waypoint waypoint) {
+    public WaypointCreatedEvent(Waypoint waypoint, Group group) {
         this.waypoint = waypoint;
+        this.group = group;
     }
 }
